a033250b3b0e17ab77f76f1f95a891f93bf24e03
nadafruksana <42529671+nadafruksana@users.noreply.github.com>
2020-08-26 12:13:42 +0530

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,26 +1,21 @@
-const express= require('express')
-const router = express.Router()
-const {authenticateUser} = require('../app/middleware/authentication')
-const tasksController = require('../app/controllers/tasksController')
-
-
-
+const express = require('express')
+const router = express.Router() 
+const { authenticateUser } = require('../app/middleware/authentication')
 const usersController = require('../app/controllers/usersController')
+const tasksController = require('../app/controllers/tasksController')
 
 router.post('/users/register', usersController.register)
 router.post('/users/login', usersController.login)
-router.get('/users/account', authenticateUser,usersController.account)
-router.delete('/users/logout',usersController.logout)
+router.get('/users/account', authenticateUser, usersController.account)
+router.delete('/users/logout',authenticateUser,usersController.logout)
 
-
-router.get('/tasks',tasksController.list)
-router.post('/tasks',tasksController.create)
+router.get('/tasks',authenticateUser, tasksController.list)
+router.get('/tasks/find',authenticateUser,tasksController.find)
+router.post('/tasks',authenticateUser,tasksController.create)
 router.get('/tasks/completed',tasksController.completed)
-router.get('/tasks/:id',tasksController.show)
-router.delete('/tasks/:id',tasksController.destroy)
-router.put('/tasks/:id',tasksController.update)
-
-
+router.get('/tasks/:id',authenticateUser,tasksController.show)
+router.delete('/tasks/:id',authenticateUser,tasksController.destroy)
+router.put('/tasks/:id',authenticateUser,tasksController.update)
 
 
 module.exports = router
