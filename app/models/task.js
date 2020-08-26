8d93a573d061448591c3b523cfb765042edb4efe
nadafruksana <42529671+nadafruksana@users.noreply.github.com>
2020-08-26 10:33:39 +0530

    Update task.js

--- app/models/task.js
+++ app/models/task.js
@@ -6,41 +6,28 @@ const taskSchema = new Schema ({
         minlength: [3,'title should more than 3 chars'],
         required:true
   },
-  description:{
-      type:String
-  },
+ 
   completed:{
         type:Boolean,
         default:false
   },
-  dueDate:{
-      type:Date,
-      //custom validations
-      validate:{
-          validator:function(value){
-              return value >=new Date()
-          },
-          message: function(){
-              return 'due Date cannot be less than today'
-          }
-      }
-  },
+  
   createdAt:{
       type:Date,
       default:Date.now
 
   },
+  expiresAt:{
+      type:Date
+  },
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     //required:true
-   
-
 }
-  
 })
-
-
 const Task = mongoose.model('Task',taskSchema)
 
 module.exports = Task
+         
+
